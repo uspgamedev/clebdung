ce6ef227c9279d5a6df01ae53cc41b0790f0eb37
Guilherme Vinicius <guilhermeferreira2529@hotmail.com>
2021-08-14 18:23:17 -0300

    Limpeza no código do minimapa

--- Godot/Cenas/Fase 1/Mapa-Tile.gd
+++ Godot/Cenas/Fase 1/Mapa-Tile.gd
@@ -5,21 +5,18 @@ onready var player = get_tree().get_root().get_node("Fase1").get_node("YSort/Pla
 
 
 func _process(_delta):
-	#Obter posição do player no minimapa
-	var playerpos_mapa = player.position/16
-	
-	#Obter tile do player no minimapa
-	var playerpos_tile = world_to_map(playerpos_mapa)
-	
-	#Atualizar posição do marcador utilizando interpolação
-	player_marker.position = (player_marker.position.linear_interpolate(playerpos_mapa, 0.08))
+	#Obter posição do player no minimapa e atualizar marcador
+	player_marker.position = player.position/16
 	
 	#Rotacionar marcador
 	
+	#Obter tile do player no minimapa
+	var playerpos_tile = world_to_map(player_marker.position)
 	
 	#Deletar tiles próximos
 	for x in range(playerpos_tile.x-3, playerpos_tile.x+4):
 		for y in range (playerpos_tile.y-3, playerpos_tile.y+4):
 				set_cell(x, y, -1)
-				
+	
+	
 	
