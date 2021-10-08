fa31a3eacd8c2bccc04c8db4867ee40309b3aef1
Guilherme Vinicius <guilhermeferreira2529@hotmail.com>
2021-10-08 19:36:19 -0300

    Animação doubt (fantasma)

--- Godot/Cenas/Fase 1/TimerGhost.gd
+++ Godot/Cenas/Fase 1/TimerGhost.gd
@@ -1,4 +1,4 @@
 extends Label
 
-func _process(delta):
+func _process(_delta):
 	set_text(str(int($Timer.get_time_left())))
--- Godot/Cenas/Inimigos/Ghost/Ghost.gd
+++ Godot/Cenas/Inimigos/Ghost/Ghost.gd
@@ -5,6 +5,7 @@ onready var player = get_tree().get_root().get_node("Fase1").get_node("YSort/Pla
 
 onready var posA = get_tree().get_root().get_node("Fase1").get_node("Positions/A")
 onready var posB = get_tree().get_root().get_node("Fase1").get_node("Positions/B")
+onready var posC = get_tree().get_root().get_node("Fase1").get_node("Positions/C")
 onready var ran1 = get_tree().get_root().get_node("Fase1").get_node("Positions/R1")
 onready var ran2 = get_tree().get_root().get_node("Fase1").get_node("Positions/R2")
 onready var ran3 = get_tree().get_root().get_node("Fase1").get_node("Positions/R3")
@@ -19,7 +20,7 @@ enum States { FOLLOW, DOUBT, RANDOM, PATROL }
 # Follow = Seguindo jogador incessantemente
 # Doubt = Estava seguindo, mas o jogador saiu do campo de visão. Vai até o
 # último local em que o jogador foi visto, espera um tempo. Volta à patrulha.
-# Patrol = Patrulha em ciclo (A -> B -> A -> B...)
+# Patrol = Patrulha em ciclo (A -> B -> C -> A -> B -> C...)
 # Random = De 30 em 30s, escolhe uma posição aleatória entre {ran1,ran2,ran3} 
 # e vai até ela (caso não esteja seguindo). Ao fim, volta a patrulhar.
 var state = States.PATROL
@@ -31,9 +32,10 @@ var tile_size = 32
 var direction = Vector2()
 var last_position = Vector2()
 var target_position = Vector2()
-var k = 0
+var k = 1
 var rng = RandomNumberGenerator.new()
 var in_sight = false
+
 
 func _ready():
 	#Inicializar variáveis e estados
@@ -54,23 +56,8 @@ func _process(delta):
 		set_direction()
 		last_position = position
 		target_position = position + (direction * tile_size)
-	#animation()
-	
-	#Animação luz
-	#Se o fantasma estava dentro do raio (k = 1) e saiu, tocar FadeOut
-	if global_position.distance_to(player.global_position) >= 98 && k == 1:
-		get_node("AnimationPlayerL").play("Light_FadeOut")
-		yield(get_node("AnimationPlayerL"), "animation_finished")
-		k = 0
-	#Se o fantasma estava fora do raio (k = 0) e entrou, tocar FadeIn
-	elif global_position.distance_to(player.global_position) < 98 && k == 0:
-		get_node("AnimationPlayerL").play("Light_FadeIn")
-		yield(get_node("AnimationPlayerL"), "animation_finished")
-		k = 1
-	#Se o fantasma permanece dentro do raio (k = 1), tocar Light animação padrão
-	elif global_position.distance_to(player.global_position) < 98 && k == 1:
-		get_node("AnimationPlayerL").play("Light")
-	
+	animation()
+
 	#Se o jogador estiver dentro da área 2D
 	if in_sight:
 		#Lançar 4 raycasts em volta do jogador
@@ -82,6 +69,9 @@ func _process(delta):
 		if !RC.is_colliding() or !RC2.is_colliding() or !RC3.is_colliding() or !RC4.is_colliding():
 			last_state = state
 			state = States.FOLLOW
+			#Caso o timer de DOUBT esteja em curso, cancelá-lo.
+			if !get_node("TimerDoubt").is_stopped():
+				get_node("TimerDoubt").stop()
 
 #Decide o que fazer no próximo frame, baseado nos possíveis estados
 func action():
@@ -90,9 +80,11 @@ func action():
 			#Terminar caminho até o jogador. Se terminado, espera um 
 			#tempo e retorna à patrulha
 			if len(path) == 0:
-				#Animação (a implementar)
-				last_state = state
-				state = States.PATROL
+				#Toca a animação de dúvida
+				get_node("AnimationPlayer").play("Doubt")
+				#Iniciar o timer para voltar à patrulha
+				if get_node("TimerDoubt").is_stopped():   #Impede início em loop
+					get_node("TimerDoubt").start()
 			return
 			
 		States.FOLLOW:
@@ -100,19 +92,29 @@ func action():
 			_update_navigation_path(global_position, player.global_position)
 			
 		States.PATROL:
-			#Se não estava em patrulha antes, calcular qual o ponto mais perto (A ou B) e
+			#Se não estava em patrulha antes, calcular qual o ponto mais perto (A, B ou C) e
 			#selecioná-lo como início do loop.
 			if last_state != States.PATROL:
-				if global_position.distance_to(posA.global_position) < global_position.distance_to(posB.global_position):
-					_update_navigation_path(global_position, posA.global_position)
-				else:
-					_update_navigation_path(global_position, posB.global_position)
+				var A = global_position.distance_to(posA.global_position)
+				var B = global_position.distance_to(posB.global_position)
+				var C = global_position.distance_to(posC.global_position)
+				var dest = min(min(A,B),C)
+				match dest:
+					A:
+						_update_navigation_path(global_position, posA.global_position)
+					B:
+						_update_navigation_path(global_position, posB.global_position)
+					C:
+						_update_navigation_path(global_position, posC.global_position)
 				last_state = States.PATROL
 			#Caso tenha alcançado o ponto A, ir ao ponto B
 			elif global_position == posA.global_position:
 				_update_navigation_path(global_position, posB.global_position)
-			#Caso tenha alcançado o ponto B, ir ao ponto A
+			#Caso tenha alcançado o ponto B, ir ao ponto C
 			elif global_position == posB.global_position:
+				_update_navigation_path(global_position, posC.global_position)
+			#Caso tenha alcançado o ponto C, ir ao ponto A
+			elif global_position == posC.global_position:
 				_update_navigation_path(global_position, posA.global_position)
 			return
 			
@@ -157,8 +159,8 @@ func _update_navigation_path(start_position, end_position):
 	path.remove(0)
 
 func animation():
+	#Animação fantasma
 	var anim_direc
-	var anim_modo
 	var animation
 	
 	match direction:
@@ -171,15 +173,25 @@ func animation():
 		Vector2(1,0):
 			anim_direc = "Right"
 		Vector2(0,0):
-			anim_direc = "Null"
+			return
 	
-	if direction != Vector2(0,0):
-		anim_modo = "Walk"
-	else:
-		anim_modo = "Idle"
-		
-	animation = anim_direc + "_" + anim_modo 
+	animation = anim_direc + "_Walk"
 	get_node("AnimationPlayer").play(animation)
+	
+	#Animação luz
+	#Se o fantasma estava dentro do raio (k = 1) e saiu, tocar FadeOut
+	if global_position.distance_to(player.global_position) >= 98 && k == 1:
+		get_node("AnimationPlayerL").play("Light_FadeOut")
+		yield(get_node("AnimationPlayerL"), "animation_finished")
+		k = 0
+	#Se o fantasma estava fora do raio (k = 0) e entrou, tocar FadeIn
+	elif global_position.distance_to(player.global_position) < 98 && k == 0:
+		get_node("AnimationPlayerL").play("Light_FadeIn")
+		yield(get_node("AnimationPlayerL"), "animation_finished")
+		k = 1
+	#Se o fantasma permanece dentro do raio (k = 1), tocar Light animação padrão
+	elif global_position.distance_to(player.global_position) < 98 && k == 1:
+		get_node("AnimationPlayerL").play("Light")
 
 
 func _on_Area2D_body_entered(body):
@@ -189,11 +201,24 @@ func _on_Area2D_body_entered(body):
 func _on_Area2D_body_exited(body):
 	if body.get_name() == "Player":
 		in_sight = false
-		last_state = state
-		state = States.DOUBT
+		if state == States.FOLLOW:
+			last_state = state
+			state = States.DOUBT
 
 
-func _on_Timer_timeout():
-	if state != States.FOLLOW:
+func _on_TimerRandom_timeout():
+	#Cancelar ida à posição aleatória caso esteja seguindo o jogador
+	if state == States.FOLLOW:
+		return
+	#Caso o fantasma esteja em DOUBT, aguardar o fim do timer
+	elif state == States.DOUBT:
+		yield(get_node("TimerDoubt"), "timeout")
+	last_state = state
+	state = States.RANDOM
+
+
+func _on_TimerDoubt_timeout():
+	#Ao fim do timer de DOUBT, caso não houve interrupção, retornar à patrulha
+	if state == States.DOUBT:
 		last_state = state
-		state = States.RANDOM
+		state = States.PATROL
