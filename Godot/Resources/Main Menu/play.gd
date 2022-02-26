be8a5feda18467050dade6b3d008f195d6a1b44e
Guilherme <guilhermeferreira2529@hotmail.com>
2022-02-26 17:43:52 -0300

    + Composição fase 2 + Remodelagem do menu principal

--- Godot/Resources/Main Menu/play.gd
+++
@@ -1,4 +0,0 @@
-extends TextureButton
-
-func _on_Play_pressed():
-	get_tree().change_scene("res://Recursos/Level Screen/level_selection.tscn")
--- Godot/Resources/Main Menu/quit.gd
+++
@@ -1,4 +0,0 @@
-extends TextureButton
-
-func _on_Quit_pressed():
-	get_tree().quit()
