ed62793574111869aa67efe88bf467b787542af2
JohnnyLima67 <64144848+JohnnyLima67@users.noreply.github.com>
2022-03-03 13:02:32 -0300

    Add traps and death time fixed

---
+++ Godot/Resources/Levels/Level 2/SandParticles.gd
@@ -0,0 +1,10 @@
+extends Particles2D
+
+onready var pos = get_parent().get_node("Position2D")
+
+func _ready():
+	self.emitting = true
+	position = pos.position
+
+func _on_Timer_timeout():
+	self.queue_free()
