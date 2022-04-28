76aef0b306b8a4f965c154baeede0f50456659e0
Lincoln-Yuji <lincolnyuji@hotmail.com>
2022-04-28 20:47:47 -0300

    Sincronizando as mudanças com o repositório remoto
    Signed-off-by: Lincoln-Yuji <lincolnyuji@hotmail.com>

--- Godot/Resources/PowerUps/PowerUpSlot.gd
+++ Godot/Resources/PowerUps/PowerUpSlot.gd
@@ -13,16 +13,9 @@ func useSlot():
 		delay = power_up_node.get_duration()
 		power_up_node.usePowerUp()
 		power_up_node = null
-<<<<<<< HEAD
 	yield(get_tree().create_timer(delay), "timeout")
 	emit_signal("used_slot")
 	return
-=======
-		yield(get_tree().create_timer(delay), "timeout")
-		emit_signal("used_slot")
-	get_parent().lock = false
-
->>>>>>> c921019c4a9d5874d7a53823100a9334f1444b06
 
 func setSlot(id):
 	var node = load("res://Resources/PowerUps/Scenes/" + id + ".tscn")
