04bdb7f1b612c0b812400b2d823d7a4418344d87
Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>
2022-08-01 17:46:16 -0300

    Susbstitua o botão Play pelo botão NewGame
    O botão NewGame, ante de carregar o primeiro nível, cria
    um novo save (sobrescreve o antigo caso exista) e então
    inicia o jogo na primeira fase.
    
    Signed-off-by: Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>

--- Godot/Resources/Main Menu/main_menu.gd
+++ Godot/Resources/Main Menu/main_menu.gd
@@ -1,21 +1,20 @@
 extends Control
 
-export(String, FILE, "*.tscn") var next_level_path
+export(String, FILE, "*.tscn") var first_level_path
 onready var settings_menu = $SettingsMenu
 
 func _ready():
 	$Fade.fade_in(1)
 
-func fade_out_and_wait():
+func _on_NewGame_pressed():
+	SaveManager.create_new_game()
 	$Fade.fade_out(1)
 	yield(get_tree().create_timer($Fade.fade_out_time), "timeout")
-
-func _on_Play_pressed():
-	fade_out_and_wait()
-	get_tree().change_scene(next_level_path)
+	get_tree().change_scene(first_level_path)
 
 func _on_Levels_pressed():
-	fade_out_and_wait()
+	$Fade.fade_out(1)
+	yield(get_tree().create_timer($Fade.fade_out_time), "timeout")
 
 func _on_Quit_pressed():
 	get_tree().quit()
@@ -26,5 +25,6 @@ func _on_SETTINGS_pressed():
 func _on_Continue_pressed():
 	var saved_level: String = str(SaveManager.load_save()["level"])
 	var level_path = "res://Resources/Levels/Level " + saved_level + "/level" + saved_level + ".tscn"
-	fade_out_and_wait()
+	$Fade.fade_out(1)
+	yield(get_tree().create_timer($Fade.fade_out_time), "timeout")
 	get_tree().change_scene(level_path)
